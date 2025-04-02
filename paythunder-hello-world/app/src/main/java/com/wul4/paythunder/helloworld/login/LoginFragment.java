f2b6bfe8e563cc76d3f059a27d89b42e83f5a852
Fran Santana <santanafran96@gmail.com>
2025-04-02 09:15:50 +0200

    Quitada la barra del action bar en el fragmento de login, arreglado problema con colores

--- paythunder-hello-world/app/src/main/java/com/wul4/paythunder/helloworld/login/LoginFragment.java
+++ paythunder-hello-world/app/src/main/java/com/wul4/paythunder/helloworld/login/LoginFragment.java
@@ -1,7 +1,7 @@
 package com.wul4.paythunder.helloworld.login;
 
 
-
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -20,9 +22,8 @@ import com.wul4.paythunder.helloworld.R;
 
 public class LoginFragment extends Fragment {
 
-   private TextInputEditText etEmail, etPassword;
-   private MaterialButton btnSignIn;
-
+    private TextInputEditText etEmail, etPassword;
+    private MaterialButton btnSignIn;
 
 
     public LoginFragment() {
@@ -30,25 +31,37 @@ public class LoginFragment extends Fragment {
     }
 
 
-
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         // Inflamos el layout
         View view = inflater.inflate(R.layout.fragment_login, container, false);
 
+
+
+        /*No funcionaba el hide del actionbar
+        ActionBar actionBar = getActivity().getActionBar();
+        if (actionBar != null){
+            actionBar.hide();
+        }
+
+        /No funciona
+        Ocultamos el boton de atras de la pantalla de login.
+        if (getActivity() instanceof AppCompatActivity) {
+            AppCompatActivity activity = (AppCompatActivity) getActivity();
+            if (activity.getSupportActionBar() != null) {
+                activity.getSupportActionBar().hide();
+            }
+        }*/
         etEmail = view.findViewById(R.id.etEmail);
         etPassword = view.findViewById(R.id.etPassword);
         btnSignIn = view.findViewById(R.id.btnSignIn);
-if (getActivity() != null){
-    //getActivity().getSupportActionBar().setTitle("Login");
-}
-        btnSignIn.setOnClickListener(v ->{
+
+        btnSignIn.setOnClickListener(v -> {
             String email = etEmail.getText().toString().trim();
             String password = etPassword.getText().toString().trim();
 
-            if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)){
+            if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
                 Toast.makeText(getContext(), "Por favor, completa todos los campos", Toast.LENGTH_SHORT).show();
             }
 
@@ -67,6 +80,17 @@ if (getActivity() != null){
         return view;
     }
 
-
-
+    /**
+     * Ocultamos tanto en el onResume como en el onStop el actionbara
+     */
+    @Override
+    public void onResume() {
+        super.onResume();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().hide();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
+    }
 }
