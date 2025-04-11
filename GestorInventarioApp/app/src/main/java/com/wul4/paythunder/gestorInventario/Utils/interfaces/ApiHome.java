17a99d3bcc83f36700ecb2e409fe25619563bc89
Monica-74 <monica.rfid@gmail.com>
2025-04-11 12:35:40 +0200

    11/04

--- GestorInventarioApp/app/src/main/java/com/wul4/paythunder/gestorInventario/Utils/interfaces/ApiHome.java
+++ GestorInventarioApp/app/src/main/java/com/wul4/paythunder/gestorInventario/Utils/interfaces/ApiHome.java
@@ -1,7 +1,12 @@
 package com.wul4.paythunder.gestorInventario.utils.interfaces;
 
+import com.wul4.paythunder.gestorInventario.entities.Producto;
+
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 /**
  * clase HomeRequest para manejar estas solicitudes, desde la API REST.
@@ -9,12 +14,16 @@ import retrofit2.http.GET;
  */
 
 public interface ApiHome {
-    @GET("api/producto")
-    Call<Integer> getProductos();
+    @GET("/listar") //tira de mi lista de produtos y los cuenta
+    Call<List<Producto>> getProductos();
 
-    //@GET("api/existencias")
-    @GET("/obtenerProductoQR/{codigo_qr}")
-    Call<Integer> getConexistencias();
+    @GET("/listar")
+    Call<List<Producto>> getConexistencias();
+
+
+//    @GET("//obtenerProductoQR/{codigo_qr}")
+//    Call<List<Producto>> getConexistencias();
+
 
     @GET("api/faltantes")
     Call<Integer> getConfaltantes();
@@ -24,4 +33,6 @@ public interface ApiHome {
 
     @GET("api/usuario/contarUsuarios")
     Call<Integer> getTotal_usuarios();
+
+
 }
--- GestorInventarioApp/app/src/main/java/com/wul4/paythunder/gestorInventario/response/HomeResponse.java
+++ GestorInventarioApp/app/src/main/java/com/wul4/paythunder/gestorInventario/response/HomeResponse.java
@@ -9,12 +9,16 @@ import com.wul4.paythunder.gestorInventario.utils.interfaces.ApiHome;
 import com.wul4.paythunder.gestorInventario.fragments.home.HomeViewModel;
 //import com.wul4.paythunder.gestorInventario.request.ApiHome;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 /**
  * La clase HomeResponse se encarga de hacer las solicitudes a la API y asignar los datos al ViewModel.
+ * Hacer las llamadas a tu API REST usando Retrofit.
+ * Actualizar los valores en tu HomeViewModel según la respuesta que reciba.
  */
 public class HomeResponse {
 
@@ -27,93 +31,107 @@ public class HomeResponse {
         this.apiHome = apiHome;
     }
 
-    public void fetchData() {
+    public void fetchAllData(){
         // Hacer las solicitudes a la API
-        fetchProductos();
-        fetchconexistencias();
-        fetchconfaltantes();
-        fetchtotal_usuarios();
+        fetchProductos(); //total de productos
+        fetchconexistencias(); // productos con existencias
+        fetchconfaltantes(); // productos sin existencia
+        fetchtotal_usuarios(); //cantidad total de usuarios
     }
 
-
-    private void fetchProductos() {
-        apiHome.getProductos().enqueue(new Callback<Integer>() {
-            @Override
-            public void onResponse(@NonNull Call<Integer> call, @NonNull Response<Integer> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    homeViewModel.setProductos(response.body());
-                } else {
-                    // Manejar el caso en que la respuesta no sea exitosa
-                    //Log.e("HomeResponse", "Fallo: " + fetchProductos().getMessage());
-                    homeViewModel.setProductos(-1); // Valor por defecto en caso de error
-                }
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<Integer> call, @NonNull Throwable t) {
-                // Manejar el error
-                homeViewModel.setProductos(-1); // Valor por defecto en caso de error
-            }
-        });
+    private void fetchAllDataProductos(){
+        Call <ListProductos>>
     }
 
-    private void fetchconexistencias() {
-        apiHome.getConexistencias().enqueue(new Callback<Integer>() {
-            @Override
-            public void onResponse(@NonNull Call<Integer> call, @NonNull Response<Integer> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    homeViewModel.setConexistencias(response.body());
-                } else {
-                    // Manejar el caso en que la respuesta no sea exitosa
-                    homeViewModel.setConexistencias(-1); // Valor por defecto en caso de error
-                }
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<Integer> call, @NonNull Throwable t) {
-                // Manejar el error
-                homeViewModel.setConexistencias(-1); // Valor por defecto en caso de error
-            }
-        });
-    }
-
-    private void fetchconfaltantes() {
-        apiHome.getConfaltantes().enqueue(new Callback<Integer>() {
-            @Override
-            public void onResponse(@NonNull Call<Integer> call, @NonNull Response<Integer> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    homeViewModel.setConfaltantes(response.body());
-                } else {
-                    // Manejar el caso en que la respuesta no sea exitosa
-                    homeViewModel.setConfaltantes(-1); // Valor por defecto en caso de error
-                }
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<Integer> call, @NonNull Throwable t) {
-                // Manejar el error
-                homeViewModel.setConfaltantes(-1); // Valor por defecto en caso de error
-            }
-        });
-    }
-
-    private void fetchtotal_usuarios() {
-        apiHome.getTotal_usuarios().enqueue(new Callback<Integer>() {
-            @Override
-            public void onResponse(@NonNull Call<Integer> call, @NonNull Response<Integer> response) {
-                if (response.isSuccessful() && response.body() != null) {
-                    homeViewModel.setTotal_usuarios(response.body());
-                } else {
-                    // Manejar el caso en que la respuesta no sea exitosa
-                    homeViewModel.setTotal_usuarios(-1); // Valor por defecto en caso de error
-                }
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<Integer> call, @NonNull Throwable t) {
-                // Manejar el error
-                homeViewModel.setTotal_usuarios(-1); // Valor por defecto en caso de error
-            }
-        });
-    }
+//    public void fetchData() {
+//        // Hacer las solicitudes a la API
+//        fetchProductos(); //total de productos
+//        fetchconexistencias(); // productos con existencias
+//        fetchconfaltantes(); // productos sin existencia
+//        fetchtotal_usuarios(); //cantidad total de usuarios
+//    }
+//
+//
+//    //aquí cuento cuantos productos llegaron
+//
+//    private void fetchProductos() {
+//        apiHome.getProductos().enqueue(new Callback<List<Producto>>() {
+//            @Override
+//            public void onResponse(@NonNull Call<List<Producto>> call, @NonNull Response<List<Producto>> response) {
+//                if (response.isSuccessful() && response.body() != null) {
+//                    homeViewModel.setProductos(response.body().size());
+//                } else {
+//                    homeViewModel.setProductos(-1);
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(@NonNull Call<List<Producto>> call, @NonNull Throwable t) {
+//                homeViewModel.setProductos(-1);
+//            }
+//        });
+//    }
+//
+//
+//
+//    private void fetchconexistencias() {
+//        apiHome.getConexistencias().enqueue(new Callback<List<Producto>>() {
+//            @Override
+//            public void onResponse(@NonNull Call<List<Producto>> call, @NonNull Response<List<Producto>> response) {
+//                if (response.isSuccessful() && response.body() != null) {
+//                    int cantidad = response.body().size(); // Contás los productos
+//                    homeViewModel.setConexistencias(cantidad); // Pasás solo el número al ViewModel
+//                } else {
+//                    // Manejar el caso en que la respuesta no sea exitosa
+//                    homeViewModel.setConexistencias(-1); // Valor por defecto en caso de error
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(@NonNull Call<List<Producto>> call, @NonNull Throwable t) {
+//                homeViewModel.setConexistencias(-1);
+//            }
+//        });
+//    }
+//
+//
+//    private void fetchconfaltantes() {
+//        apiHome.getConfaltantes().enqueue(new Callback<Integer>() {
+//            @Override
+//            public void onResponse(@NonNull Call<Integer> call, @NonNull Response<Integer> response) {
+//                if (response.isSuccessful() && response.body() != null) {
+//                    homeViewModel.setConfaltantes(response.body());
+//                } else {
+//                    // Manejar el caso en que la respuesta no sea exitosa
+//                    homeViewModel.setConfaltantes(-1); // Valor por defecto en caso de error
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(@NonNull Call<Integer> call, @NonNull Throwable t) {
+//                // Manejar el error
+//                homeViewModel.setConfaltantes(-1); // Valor por defecto en caso de error
+//            }
+//        });
+//    }
+//
+//    private void fetchtotal_usuarios() {
+//        apiHome.getTotal_usuarios().enqueue(new Callback<Integer>() {
+//            @Override
+//            public void onResponse(@NonNull Call<Integer> call, @NonNull Response<Integer> response) {
+//                if (response.isSuccessful() && response.body() != null) {
+//                    homeViewModel.setTotal_usuarios(response.body());
+//                } else {
+//                    // Manejar el caso en que la respuesta no sea exitosa
+//                    homeViewModel.setTotal_usuarios(-1); // Valor por defecto en caso de error
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(@NonNull Call<Integer> call, @NonNull Throwable t) {
+//                // Manejar el error
+//                homeViewModel.setTotal_usuarios(-1); // Valor por defecto en caso de error
+//            }
+//        });
+//    }
 }
