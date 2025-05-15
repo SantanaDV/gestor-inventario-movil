9529190e5c797554faa1e4697f086682ac4d1689
Monica-74 <monica.rfid@gmail.com>
2025-05-15 08:24:10 +0200

    Refactor: actualiza adapter y fragmento de tareas

--- GestorInventarioApp/app/src/main/java/com/wul4/paythunder/gestorInventario/Utils/interfaces/ApiTarea.java
+++ GestorInventarioApp/app/src/main/java/com/wul4/paythunder/gestorInventario/Utils/interfaces/ApiTarea.java
@@ -5,7 +5,9 @@ import com.wul4.paythunder.gestorInventario.entities.Tarea;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 /**
  * clase ApiTarea para manejar solicitudes de autenticación, desde la API REST.
@@ -13,9 +15,8 @@ import retrofit2.http.GET;
  */
 
 public interface ApiTarea {
-    @GET("api/tarea")
-    Call<List<Tarea>> getCrearTareas();
-
+//    @POST("api/tarea/guardarTarea")
+//    Call<Tarea> getguardarTarea();
     @GET("api/tarea/totalTareas")
     Call<List<Tarea>> gettotalTareas();
 
@@ -29,5 +30,15 @@ public interface ApiTarea {
     Call<List<Tarea>> gettareasRealizadas();
 
 
+    @POST("api/tarea/crearTarea")  // Endpoint para crear una nueva tarea, por ello utilizamos @POST en vez de @GET
+    Call<Tarea>crearTarea(@Body Tarea nuevaTarea);  // Usamos @Body para enviar el objeto en el cuerpo
 
+    @POST("api/tarea/eliminarTarea")
+    Call<Tarea> eliminarTarea(int id);
+
+    @POST("api/tarea/actualizarTarea")
+    Call<Tarea> actualizarTarea(Tarea id);
+
+    @POST("api/tarea/crearTareas")
+    Call<List<Tarea>> getCrearTareas();
 }
--- GestorInventarioApp/app/src/main/java/com/wul4/paythunder/gestorInventario/entities/Tarea.java
+++ GestorInventarioApp/app/src/main/java/com/wul4/paythunder/gestorInventario/entities/Tarea.java
@@ -9,22 +9,23 @@ public class Tarea {
     private int estado;
     private String  descripcion;
     private String empleado_asignado;
-    private Date fecha_asignacion;
-    private Date fecha_finalizacion;
+    private String fecha_asignacion;
+    private String fecha_finalizacion;
     private int id_categoria;
 
 
-    public Tarea(int id) {
-    }
-
-    public Tarea(int id, int estado, String descripcion, String empleado_asignado, Date fecha_asignacion, Date fecha_finalizacion, int id_categoria) {
+    public Tarea(int id, int estado, String descripcion, String empleado_asignado, String fecha_finalizacion, Date fecha_asignacion, int id_categoria) {
         this.id = id;
         this.estado = estado;
         this.descripcion = descripcion;
         this.empleado_asignado = empleado_asignado;
-        this.fecha_asignacion = fecha_asignacion;
         this.fecha_finalizacion = fecha_finalizacion;
+        this.fecha_asignacion = String.valueOf(fecha_asignacion);
         this.id_categoria = id_categoria;
+    }
+
+    public Tarea() {
+
     }
 
     public int getId() {
@@ -48,19 +49,17 @@ public class Tarea {
         this.empleado_asignado = empleado_asignado;
     }
 
-
-    public void setFecha_asignacion(Date fecha_asignacion) {
-        this.fecha_asignacion = fecha_asignacion;
+    public CharSequence getFecha_asignacion() {
+        return (CharSequence) fecha_asignacion;
     }
+
 
 
     public void setEstado(int estado) {
         this.estado = estado;
     }
 
-    public void setFecha_finalizacion(Date fecha_finalizacion) {
-        this.fecha_finalizacion = fecha_finalizacion;
-    }
+
 
     public int getId_categoria() {
         return id_categoria;
@@ -70,12 +69,16 @@ public class Tarea {
         return empleado_asignado;
     }
 
-    public Date getFecha_asignacion() {
-        return fecha_asignacion;
+    public void setFecha_asignacion(String fecha_asignacion) {
+        this.fecha_asignacion = fecha_asignacion;
     }
 
-    public Date getFecha_finalizacion() {
-        return fecha_finalizacion;
+    public CharSequence getFecha_finalizacion() {
+        return (CharSequence) fecha_finalizacion;
+    }
+
+    public void setFecha_finalizacion(String fecha_finalizacion) {
+        this.fecha_finalizacion = fecha_finalizacion;
     }
 
     public void setId_categoria(int id_categoria) {
@@ -90,6 +93,7 @@ public class Tarea {
     public int getEstado() {
         return estado ;
     }
+
 
 
 }
